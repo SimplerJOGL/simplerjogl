401110d5c145b55ad4713a3e1c5048367d5f9cf6
Seth Battis <seth@battis.net>
2011-01-26 20:31:00 +0000

    Updated documentation of Sprite to be more end-user (student) readable. Tweaked sprite to (when drawn using the Model.draw() method) draw relative to it's (x,y,z) coordinates -- this may break older code that relies on Model.draw() not doing this!)

--- src/simplerjogl/sprites/Sprite.java
+++ src/simplerjogl/sprites/Sprite.java
@@ -5,15 +5,34 @@ import javax.media.opengl.*;
 
 import simplerjogl.*;
 
+/**
+ * A basic Sprite object, which extends the SimplerJOGL.Model object to
+ * include an object center at (x, y, z). Shifting the center will shift the
+ * whole sprite relative to that point.
+ * 
+ * @author <a href="mailto:seth@battis.net">Seth Battis</a>
+ * @version 2011-01-26
+ */
 public abstract class Sprite extends Model
 {
 	protected double x, y, z;
 
+	/**
+	 * The default constructor
+	 * @param gl the OpenGL drawing context in which this sprite exists
+	 */
 	public Sprite (GL2 gl)
 	{
 		this (gl, 0, 0, 0);
 	}
 
+	/**
+	 * Construct a Sprite at specific (x, y, z) coordinates
+	 * @param gl the OpenGL drawing context in which this sprite exists
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @param z z-coordinate
+	 */
 	public Sprite (GL2 gl, double x, double y, double z)
 	{
 		super (gl);
@@ -22,26 +41,48 @@ public abstract class Sprite extends Model
 		this.z = z;
 	}
 
+	/**
+	 * This method overrides the basic Model.draw() method, drawing the sprite
+	 * at (or, at least, relative to) its (x, y, z) center point.
+	 */
 	public void draw ()
 	{
-		spriteDraw ();
+		gl.glPushMatrix();
+		{
+			gl.glTranslated (x, y, z);
+			spriteDraw ();
+		}
+		gl.glPopMatrix();
 	}
 
+	/**
+	 * @return x-coordinate
+	 */
 	public double getX ()
 	{
 		return x;
 	}
 
+	/**
+	 * @return y-coordinate
+	 */
 	public double getY ()
 	{
 		return y;
 	}
 
+	/**
+	 * @return z-coordinate
+	 */
 	public double getZ ()
 	{
 		return z;
 	}
 
+	/**
+	 * @param newX new x-coordinate
+	 * @return old x-coordinate
+	 */
 	public double setX (double newX)
 	{
 		double oldX = x;
@@ -49,6 +90,10 @@ public abstract class Sprite extends Model
 		return oldX;
 	}
 
+	/**
+	 * @param newY new y-coordinate
+	 * @return old y-coordinate
+	 */
 	public double setY (double newY)
 	{
 		double oldY = y;
@@ -56,6 +101,10 @@ public abstract class Sprite extends Model
 		return oldY;
 	}
 
+	/**
+	 * @param newZ new z-coordinate
+	 * @return old z-coordinate
+	 */
 	public double setZ (double newZ)
 	{
 		double oldZ = z;
@@ -63,7 +112,26 @@ public abstract class Sprite extends Model
 		return oldZ;
 	}
 
+	/**
+	 * This abstract method (which must be implemented in any class that
+	 * extends the Sprite class) is called to animate the sprite. Depending on
+	 * how the sprite is animated, a JOGLRenderer may call this method when
+	 * the display is refreshed (easy, but animation speeds will vary based on
+	 * display refresh rates and processor speeds) or based on a timer (more
+	 * elegant -- like a lightsaber -- but needing to be implemented either in
+	 * the Sprite itself (very elegant) or in the JOGLRenderer.
+	 */
 	public abstract void move ();
 
+	/**
+	 * This abstract method (which must be implemented in any class that
+	 * extends the Sprite class) is called to draw the Sprite centered at (or,
+	 * at least, relative to) the origin. The spriteDraw() method is called by
+	 * the generic draw() method. The draw() method will handle all
+	 * translations relative to the center (x, y, z) coordinate of Sprite --
+	 * i.e. spriteDraw() does not need to worry about drawing relative to the
+	 * Sprite's center, spriteDraw() does need to draw the Sprite relative to
+	 * the origin.
+	 */
 	public abstract void spriteDraw ();
 }
