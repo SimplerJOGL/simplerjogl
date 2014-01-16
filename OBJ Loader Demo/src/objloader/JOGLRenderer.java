aef395cf7e893d4002e02d4983da43db4d5a6c35
Seth Battis <SethBattis@stmarksschool.org>
2014-01-16 14:55:31 -0500

    Demo Wireframe OBJ
    Demonstrate the wireframe drawing of OBJ files.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- OBJ Loader Demo/src/objloader/JOGLRenderer.java
+++ OBJ Loader Demo/src/objloader/JOGLRenderer.java
@@ -9,7 +9,6 @@ import simplerjogl.loader.obj.*;
 public class JOGLRenderer extends Renderer
 {
 	private LoadedObjModel suzanne;
-	private Light light;
 	private float rot;
 	
 	public void init ()
@@ -19,9 +18,7 @@ public class JOGLRenderer extends Renderer
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		light = new Light(gl);
-		light.enable();
+
 		rot = 0;
 	}
 
@@ -29,6 +26,6 @@ public class JOGLRenderer extends Renderer
 	{
 		glu.gluLookAt (0, 0, 5, 0, 0, 0, 0, 1, 0);
 		gl.glRotatef(rot++, 0, 1, 0);
-		suzanne.draw();
+		suzanne.wireframe();
 	}
 }
